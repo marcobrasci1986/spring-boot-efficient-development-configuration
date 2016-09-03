68c0ce89752b18d911a38262d653d7563601e5cc
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-09-03 13:54:07 +0200

    Adding Custom Auto configuration

--- src/main/java/schultz/dustin/io/JustGifItApplication.java
+++ src/main/java/schultz/dustin/io/JustGifItApplication.java
@@ -2,11 +2,30 @@ package schultz.dustin.io;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.WebSocketAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.filter.HttpPutFormContentFilter;
+import org.springframework.web.filter.RequestContextFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+/**
+ * @Configuration
+ * @ComponentScan
+ * @EnableAutoConfiguration
+ */
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class, JmxAutoConfiguration.class, WebSocketAutoConfiguration.class})
 public class JustGifItApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JustGifItApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustGifItApplication.class, args);
+    }
+
+
 }
--- src/main/java/schultz/dustin/io/controller/UploadController.java
+++ src/main/java/schultz/dustin/io/controller/UploadController.java
@@ -1,5 +1,12 @@
 package schultz.dustin.io.controller;
 
+
+import com.justgifit.services.ConverterService;
+import com.justgifit.services.GifEncoderService;
+import com.justgifit.services.VideoDecoderService;
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,9 +14,12 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * @author mbrasci
@@ -20,20 +30,39 @@ public class UploadController {
     private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Value("${multipart.location}")
-    private String locataion;
+    private String location;
 
-    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE)
+    @Inject
+    private ConverterService converterService;
+
+    @Inject
+    private GifEncoderService gifEncoderService;
+
+    @Inject
+    private VideoDecoderService videoDecoderService;
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces =
+            MediaType.IMAGE_GIF_VALUE)
     public String upload(@RequestPart("file") MultipartFile file,
                          @RequestParam("start") int start,
                          @RequestParam("end") int end,
                          @RequestParam("speed") int speed,
-                         @RequestParam("repeat") boolean repeat) throws IOException {
-
-        File videoFile = new File(locataion + "/" + System.currentTimeMillis() + ".mp4");
+                         @RequestParam("repeat") boolean repeat) throws IOException, FrameGrabber.Exception {
+        File videoFile = new File(location + "/" + System
+                .currentTimeMillis() + ".mp4");
         file.transferTo(videoFile);
 
-        log.info("Saved file to {} ", videoFile.getAbsoluteFile());
+        log.info("Saved video file to {}", videoFile.getAbsolutePath());
 
-        return "";
+        Path output = Paths.get(location + "/gif/" + System.currentTimeMillis() + ".gif");
+
+        FFmpegFrameGrabber frameGrabber = videoDecoderService.read(videoFile);
+        AnimatedGifEncoder gifEncoder = gifEncoderService.getGifEncoder(repeat,
+                (float) frameGrabber.getFrameRate(), output);
+        converterService.toAnimatedGif(frameGrabber, gifEncoder, start, end, speed);
+
+        log.info("Saved generated gif to {}", output.toString());
+
+        return output.getFileName().toString();
     }
 }
